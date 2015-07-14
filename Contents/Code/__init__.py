6e0af1f04dbf5c9a5a59b8f94dfc5614137029e9
GadgetReactor <sean@gadgetreactor.com>
2015-07-14 09:35:48 +0800

    1.6.1

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# last updated 2014-09-06
+# last updated 2015-07-14
 # coded by Sean S (GadgetReactor)
-# visit www.gadgetreactor.com/portfolio/sgtv for updates 
+# visit www.gadgetreactor.com/portfolio/sgtv for updates
 
 NAME = 'SG!tv'
 
@@ -28,7 +28,7 @@ def openUrl(url):
 			content.close()
 			data = str(data).replace('\n','')
 			return data
-			
+
 		except urllib2.HTTPError,e:
 			retries += 1
 			print ' - Retries: ' + str(retries) + str(e.code)
@@ -56,7 +56,7 @@ def MainMenu():
 
 	oc = ObjectContainer()
 
-	oc.add(DirectoryObject(key = Callback(Episodes, title = 'Recently Released', channel='new', show='all'), title = 'Recently Released'))
+	oc.add(DirectoryObject(key = Callback(Toggle, channel='catchup-listing'), title = 'Recently Released'))
 	oc.add(DirectoryObject(key = Callback(Shows, channel='channel5'), title = 'Channel 5', thumb=R('channel5.jpg')))
 	oc.add(DirectoryObject(key = Callback(Shows, channel='channel8'), title = 'Channel 8', thumb=R('channel8.jpg')))
 	oc.add(DirectoryObject(key = Callback(Shows, channel='channelu'), title = 'Channel U', thumb=R('channelu.jpg')))
@@ -66,14 +66,14 @@ def MainMenu():
 	oc.add(DirectoryObject(key = Callback(Youtube, user='channelnewsasia'), title = 'Channel News Asia', thumb=R('cna.png')))
 	oc.add(DirectoryObject(key = Callback(Youtube, user='wahbanana'), title = 'WahBanana', thumb=R('wahbanana.jpg')))
 	oc.add(DirectoryObject(key = Callback(Viddsee, page='0', type = 'popular'), title = 'Viddsee', thumb=R('viddsee.png')))
-	
+
 	return oc
 
 ###################################################################################################
 
 def htmlParse(str):
 	str=str.replace('\\x3a', ':')
-	str=str.replace('\\x2f', '/')	
+	str=str.replace('\\x2f', '/')
 	str=re.sub('&amp;','&',str)
 	str=re.sub('&#39;',"'",str)
 	str=str.replace('&quot;', '"')
@@ -82,27 +82,70 @@ def htmlParse(str):
 	str=re.sub(r'<.*?>','', str)
 	return str
 
-###################################################################################################		
-		
+def Thumb(url):
+	try:
+		data = HTTP.Request(url, cacheTime = CACHE_1MONTH).content
+		return DataObject(data, 'image/jpeg')
+	except:
+		return Redirect(R(ICON))
+
+
+###################################################################################################
+
+def Toggle(channel):
+	oc = ObjectContainer()
+	data=openUrl("http://video.toggle.sg/en/%s" % channel)
+	showlist  = re.compile('<div class="tg-teaser-item".*?data-srcset="(.+?)".+?href="(.+?)">(.+?)<').findall(data)
+
+	for image, url, show in showlist:
+		if "http" not in image:
+			image = "http://video.toggle.sg" + image
+		video = VideoClipObject(
+								title = show,
+								#originally_available_at = video_date,
+								#rating = video_rating,
+								thumb = Callback(Thumb, url=image),
+								url = url,
+								)
+
+		oc.add(video)
+	return oc
+
+
+###################################################################################################
+
 def Shows(channel):
 	oc = ObjectContainer()
-	data = openUrl("http://xin.msn.com/en-sg/video/catchup/")
-	showlist  = re.compile('<div data-tabkey="tab-(\d+)".*?homepage\|%s\|tab\|(.*?)\|.+?:&quot;(.+?)&quot' % (channel)).findall(data)
+	data=openUrl("http://tv.toggle.sg/en/%s/shows" % channel)
+	showlist  = re.compile('data-src.+?"(.+?)".+?href="(.+?)">(.+?)<').findall(data)
 
-	for tab, show, thumb in showlist:
-		image = 'http:'+ thumb.replace('&amp;','&')
-		oc.add(DirectoryObject(key = Callback(Episodes, show=show, channel=channel, tab=tab), title = show))	
+	"""
+	Channel 8
+	id: 5275960,
+    navigationId: 5013778,
+    channelId: 331027,
+
+	Channel 5
+	id: 5275964,
+	navigationId: 5013778,
+	channelId: 331025,
+	"""
+
+	for image, url, show in showlist:
+		if "http" not in image:
+			image = "http://video.toggle.sg" + image
+		oc.add(DirectoryObject(key = Callback(Episodes, show=show, channel=channel, tab=url, page=0), title = show, thumb = Callback(Thumb, url=image)))
 
 	return oc
 
-###################################################################################################	
+###################################################################################################
 
 def Youtube(user):
 	oc = ObjectContainer()
 	youtube_url = "http://gdata.youtube.com/feeds/api/users/" + user + "/uploads?v=2&alt=json"
 
 	data = JSON.ObjectFromURL (youtube_url)
-		
+
 	for entry in data["feed"]["entry"]:
 		title = entry["title"]["$t"]
 		video_id = entry["media$group"]["yt$videoid"]["$t"]
@@ -113,18 +156,19 @@ def Youtube(user):
 								summary = desc,
 								#originally_available_at = video_date,
 								#rating = video_rating,
-								url = "http://youtube.com/watch?v="+video_id
+
+								url = "http://youtube.com/watch?v="+video_id,
 								)
-		
+
 		oc.add(video)
 	return oc
-			
-###################################################################################################	
+
+###################################################################################################
 
 def Viddsee(page, type):
 	oc = ObjectContainer()
-	
-#	type examples: | popular | genre/drama | genre/comedy |  
+
+#	type examples: | popular | genre/drama | genre/comedy |
 	viddsee_url = "https://www.viddsee.com/v1/browse/"+type+"?current_page="+page+"&per_page=12"
 	data = JSON.ObjectFromURL(viddsee_url)
 
@@ -134,56 +178,58 @@ def Viddsee(page, type):
 		Log (video_url)
 		print video_url
 		video = VideoClipObject(
-								title = video["title"], 
+								title = video["title"],
 								#summary = htmlParse(video["description_long"]),
 								#tagline = video["description_short"],
 								#genres = video["genres"],
 								#duration = video["duration"],
 								#rating = float (video["rating"]["rating_like"]),
 								#year = video["year"],
-								#thumb = video["thumbnail_url"],
+								thumb = Callback(Thumb, url=video["thumbnail_url"]),
 								#art = video["photo_large_url"],
-								url =  video_url
-								)	
+								url =  video_url,
+								)
 		oc.add(video)
-		
+
 	page = str(int(page)+1)
 	oc.add(DirectoryObject(key = Callback(Viddsee, page=page, type = 'popular'), title = 'Viddsee', thumb=R('viddsee.png')))
 	return oc
-	
-###################################################################################################	
-def Episodes(show, channel, tab):
+
+###################################################################################################
+def Episodes(show, channel, tab, page):
 	oc = ObjectContainer()
 
-	data=openUrl("http://xin.msn.com/en-sg/video/catchup/")
+	data=openUrl(tab + "/episodes")
+	meta = re.compile('10, 0,  (.+?), (.+?), (.+?)\);',re.DOTALL).search(data)
 
-	if "new" in channel:
-		episodelist = re.compile('<li.+?href="(.+?)".+?:&quot;(.+?)&quot.+?<h4>(.+?)</h4>.+?"duration">(.+?)<.+?</li>').findall(data)	
+	data = openUrl ("http://tv.toggle.sg/en/blueprint/servlet/toggle/paginate?pageSize=10&pageIndex="+str(page)+"&contentId="+meta.group(1)+"&navigationId="+meta.group(2)+"&"+meta.group(3)+"=1")
 
-	else:
-		episodechunk  = re.compile('class="section tabsection horizontal".+?data-section-id="%s".+?<div data-tabkey="%s"(.+?)</ul>' % (channel, tab)).search(data).group(1)
-		episodelist = re.compile('<li.+?href="(.+?)".+?:&quot;(.+?)&quot.+?<h4>(.+?)</h4>.+?"duration">(.+?)<.+?</li>').findall(episodechunk)	
+	episodechunk  = re.compile('<li>(.*?)<\/li>').findall(data)
 
-	for episode_url, thumb, title, time in episodelist:
-		episode_url = "http://xin.msn.com" + episode_url
-		title=htmlParse(title)									
-		
-		image = 'http:'+ thumb.replace('&amp;','&')
-		infoLabels={'Title': title, 'Duration':time}
-		#oc.add(DirectoryObject(key = Callback(playBack, title = title, url = episode_url), title = title))
-		#oc.add(CreateObject(url=Callback(playBack2, url=episode_url), media_type="video", title=title))
-		thumbUrl = "http://img1.catalog.video.msn.com/Image.aspx?uuid=" + thumb + "&w=400&h=300"
-		#mo = MediaObject(parts=[PartObject(key=HTTPLiveStreamURL(Callback(GetVideo, url=episode_url, channel=channel)))])
-		#rating_key = episode_url
-		#vco = VideoClipObject(key=title, rating_key=rating_key, title=title)
-		video = VideoClipObject(
-								title = title,
-								#summary = video_summary,
-								#originally_available_at = video_date,
-								#rating = video_rating,
-								url = episode_url
-								)
-		
-		oc.add(video)
-		
+	for chunk in episodechunk:
+		episodelist = re.compile('img src="(.+?)".+?.+?item__tags(.+?)<\/div>.+?href="(.+?)">(.+?)<\/a>.+?<p>(.+?)<\/p>').search(chunk)
+		vip_status = episodelist.group(2)
+		if "vip" not in vip_status:
+			episode_url = episodelist.group(3)
+			title = str(episodelist.group(4))
+			desc = episodelist.group(5)
+			image = episodelist.group(1)
+
+			video = VideoClipObject(
+							title = title,
+							#summary = video_summary,
+							#originally_available_at = video_date,
+							#rating = video_rating,
+							thumb = Callback(Thumb, url=image),
+							url = episode_url,
+							)
+
+			oc.add(video)
+
+	pagination = re.compile ('pagination\'\),(.+?), (.+?), paginateLabel').search(data)
+	current_page = int( pagination.group (1) )
+	max_page = int ( pagination.group (2) )
+	if current_page < max_page:
+		page = str(current_page + 1)
+		oc.add(DirectoryObject(key = Callback(Episodes, show=show, channel=channel, tab=tab, page=page), title = 'Next Page', ))
 	return oc
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ SGtv.bundle
 ===========
 
 SGTV Plugin / Addon for Plex. Allows watching of Singapore content from Mediacorp Channels, Channel News Asia, Viddsee and Singapore Youtube Channels.
+
+Last Updated 2015-07-14
+Same feature set as SG!TV 1.6.1 XBMC / KODI Plugin
